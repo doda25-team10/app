6fbab3bebdd6f48e6e74d1b973e98b1c35280e1e
Ciamek27 <m.a.wierzbicki@student.vu.nl>
2025-11-21 17:20:04 +0100

    added lib-version as dependency and called it in the HelloWorldController

--- src/main/java/frontend/ctrl/HelloWorldController.java
+++ src/main/java/frontend/ctrl/HelloWorldController.java
@@ -1,5 +1,6 @@
 package frontend.ctrl;
 
+import dev.doda.team.lib.VersionUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,7 @@ public class HelloWorldController {
     @GetMapping("/")
     @ResponseBody
     public String index() {
-        return "Hello World!";
+		var version = new VersionUtil().getVersion();
+		return "Hello World! lib-version=" + version;
     }
 }
